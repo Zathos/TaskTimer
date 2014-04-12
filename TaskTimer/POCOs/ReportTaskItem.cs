47923f617ef040321464af124a94672945626fa1
Zathos <derrin.boerschel@gmail.com>
2014-04-11 21:55:55 -0500

    Added missing file.

---
+++ TaskTimer/POCOs/ReportTaskItem.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace TaskTimer.POCOs
+{
+    public class ReportTaskItem
+    {
+        public ReportTaskItem(string date, IList<TaskItem> taskItems)
+        {
+            Date = date;
+            TaskItems = taskItems;
+        }
+
+        public string Date { get; private set; }
+        public IList<TaskItem> TaskItems { get; private set; }
+    }
+}
